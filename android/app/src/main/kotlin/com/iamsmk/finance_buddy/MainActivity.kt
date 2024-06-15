81b5dd04256c16ff91744cfb33a20e11a7ec4553
Selvamuthukumaran Rajan <selvamuthu452000@gmail.com>
2024-06-15 22:27:06 +0530

    source commit

---
+++ android/app/src/main/kotlin/com/iamsmk/finance_buddy/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.iamsmk.finance_buddy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/features/splash/presentation/splash_screen.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+import 'package:finance_buddy/features/home/presentation/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 4), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (_) => const HomeScreen(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Icon(
+          Icons.fingerprint_rounded,
+        ),
+      ),
+    );
+  }
+}
